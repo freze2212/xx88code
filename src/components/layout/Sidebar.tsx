523139b547460dec0b517b40268b3e0a6ec83094
michael <michael@itkjc.com>
2025-10-28 20:54:54 +0700

    link mobi

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,28 +1,51 @@
 import Image from 'next/image';
-
+import Link from 'next/link';
 export function Sidebar() {
   const menuItems = [
-    { name: 'Nhập Code', icon: '/images/nhap-code.png', bgColor: 'bg-orange-500' },
-    { name: 'Liên Hệ', icon: '/images/lien-he.png', bgColor: 'bg-green-500' },
-    { name: 'Telegram', icon: '/images/telegram.png', bgColor: 'bg-blue-400' },
-    { name: 'Fanpage', icon: '/images/fanpage.png', bgColor: 'bg-blue-600' },
-    { name: 'Trang Chủ', icon: '/images/home.png', bgColor: 'bg-blue-400' },
+    {
+      name: 'Liên Hệ',
+      icon: '/images/lien-he.png',
+      bgColor: 'bg-green-500',
+      linkTo: 'https://xx88cskh.pages.dev/',
+    },
+    {
+      name: 'Telegram',
+      icon: '/images/telegram.png',
+      bgColor: 'bg-blue-400',
+      linkTo: 'https://t.me/GIAITRIXX88_official',
+    },
+    {
+      name: 'Fanpage',
+      icon: '/images/fanpage.png',
+      bgColor: 'bg-blue-600',
+      linkTo: 'https://www.facebook.com/XX88official.com.vn/',
+    },
+    {
+      name: 'Trang Chủ',
+      icon: '/images/home.png',
+      bgColor: 'bg-blue-400',
+      linkTo: 'https://5333.com/',
+    },
   ];
 
   return (
-    <div className="bg-white py-2 border-b border-gray-100 md:hidden">
-      <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="border-b border-gray-100 bg-white py-2 md:hidden">
+      <div className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex justify-center space-x-4 sm:space-x-6 lg:space-x-8">
           {menuItems.map((item, index) => (
             <div key={index} className="flex flex-col items-center space-y-2">
-              <Image
-                src={item.icon}
-                alt={item.name}
-                width={40}
-                height={40}
-                className="w-10 h-10"
-              />
-              <span className="text-xs font-medium text-gray-700 whitespace-nowrap">{item.name}</span>
+              <Link href={item.linkTo || '#'}>
+                <Image
+                  src={item.icon}
+                  alt={item.name}
+                  width={40}
+                  height={40}
+                  className="h-10 w-10"
+                />
+              </Link>
+              <span className="whitespace-nowrap text-xs font-medium text-gray-700">
+                {item.name}
+              </span>
             </div>
           ))}
         </div>
